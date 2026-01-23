1c9c3c2a5e7c8570747d39a1105175cd8da42734
Pro203S <prosirea2501@gmail.com>
2026-01-23 21:02:08 +0900

    기본 제공 파일 삭제

--- middleware.ts
+++
@@ -1,7 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-export async function Middleware(req: Request, res: Response, next: NextFunction) {
-    next();
-}
-
-export const matches = "/*";
