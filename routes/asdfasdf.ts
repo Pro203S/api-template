82d991a730734536292a33df2dd70693b7f52f7a
Pro203S <prosirea2501@gmail.com>
2026-02-20 04:46:54 +0900

    기본 루트 파일 수정

--- routes/asdfasdf.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-
-export async function GET(req: Request, res: Response) {
-    return res.status(200).send("Hello World!");
-}
--- routes/index.ts
+++ routes/index.ts
@@ -1,5 +1,13 @@
 import { Request, Response } from "express";
 
 export async function GET(req: Request, res: Response) {
-    return res.status(200).send("Hello World!");
+    try {
+        return res.status(200).send("Hello World!");
+    } catch (err) {
+        const e = err as Error;
+        return res.status(500).json({
+            "code": 500,
+            "message": e.message
+        });
+    }
 }
